421c536ba687d2dfe33dfc78d022ad5a845461f3
Ariz Arshal <arizarshal@gmail.com>
2023-05-06 23:06:24 +0530

    done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer()
 require('dotenv').config()
 
 var app = express();
@@ -12,6 +14,16 @@ app.get('/', function (req, res) {
 });
 
 
+// post image middleware 
+
+
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  })
+})
 
 
 const port = process.env.PORT || 3000;
